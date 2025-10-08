a941119463ac7d660bf9ef7ef13a273c5bec8c90
Joseph Yi <yijose@myvuw.ac.nz>
2025-10-08 17:10:54 +1300

    Initial Commit
    Please work

---
+++ Scripts/Global.gd
@@ -0,0 +1,79 @@
+extends Node
+
+var bodies_collection = {
+	"01" : preload("res://Assets/body.png")
+}
+
+var hair_collection = {
+	"none" : null, 
+	"01" : preload("res://Assets/hair.png"),
+	"02" : preload("res://Assets/hair2.png"),
+
+}
+var outfit_collection = {
+	"01" : preload("res://Assets/outfitdress.png"),
+	"02" : preload("res://Assets/outfitsuit.png"),
+}
+
+#big collections for char creator
+var bodies_collection_BIG = {
+	"01" : preload("res://Assets/big assets/basespritesheet.png")
+}
+
+var hair_collection_BIG = {
+	"none" : null, 
+	"01" : preload("res://Assets/big assets/hairspritesheet.png"),
+	"02" : preload("res://Assets/big assets/ponytailsprite.png"),
+
+}
+var outfit_collection_BIG = {
+	"01" : preload("res://Assets/big assets/dressspritesheet.png"),
+	"02" : preload("res://Assets/big assets/suitspritesheet.png"),
+}
+
+
+var body_colour_options = [
+	Color(0.96, 0.88, 0.69), #light skin
+	Color(0.72, 0.54, 0.39), #medium skin
+	Color(0.45, 0.34, 0.27), #brown skin
+]
+
+var hair_colour_options = [
+	Color(0.1, 0.1, 0.1), #Black
+	Color(0.4, 0.2, 0.1), #Brown
+	Color(0.9, 0.6, 0.2), #Blonde
+	Color(0.5, 0.25, 0), #Aubrun
+	Color(0.9, 0.8, 1), #Greyish Purple?
+]
+
+var colour_options = [
+	Color(1, 1, 1), #Default
+	Color(1, 0, 0), #Red
+	Color(0, 1, 0), #Green
+	Color(0, 0, 1), #Blue
+	Color(0, 0, 0), #Black
+	Color(1, 1, 1), #White
+]
+
+#selected values
+var selected_body = ""
+var selected_hair = ""
+var selected_outfit = ""
+
+#big for character creation
+var selected_body_BIG = ""
+var selected_hair_BIG = ""
+var selected_outfit_BIG = ""
+
+var selected_body_colour_BIG = ""
+var selected_hair_colour_BIG = ""
+
+######
+
+var selected_body_colour = ""
+var selected_hair_colour = ""
+
+
+var player_name = ""
+
+
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,21 +1,96 @@
-class_name player extends CharacterBody2D
+extends CharacterBody2D
 
-var move_speed : float = 300.0
+# Node References
+@onready var body = $Skeleton/body
+@onready var hair = $Skeleton/hair
+@onready var outfit = $Skeleton/outfit
 
+# AnimationPlayer node
+@onready var animation_player = $AnimationPlayer
 
-# Called when the node enters the scene tree for the first time.
+var last_direction = Vector2.ZERO
+const speed = 200
+
 func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	var direction : Vector2 = Vector2.ZERO
-	direction.x = Input.get_action_strength("right") - Input.get_action_strength("left")
-	direction.y = Input.get_action_strength("down") - Input.get_action_strength("up")
-
-	velocity = direction * move_speed
-	pass
+	initialize_player()
 
 func _physics_process(delta):
+	var direction = Vector2.ZERO
+	direction.x = Input.get_axis("ui_left", "ui_right")
+	direction.y = Input.get_axis("ui_up", "ui_down")
+	
+	# store last direction
+	if direction != Vector2.ZERO:
+		last_direction = direction
+	
+	# update direction and velocity 
+	if direction.x != 0:
+		velocity.x = direction.x * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed * delta)
+		
+	if direction.y != 0:
+		velocity.y = direction.y * speed
+	else:
+		velocity.y = move_toward(velocity.y, 0, speed * delta)
+		
+	# pick animation
+	var next_anim = ""
+	if direction.x != 0:
+		# reuse walk_left but flip if going right
+		next_anim = "walk_left"
+		set_flip(direction.x > 0)
+	elif direction.y < 0:
+		next_anim = "walk_up"
+		set_flip(false)
+	elif direction.y > 0:
+		next_anim = "walk_down"
+		set_flip(false)
+	elif direction == Vector2.ZERO:
+		if last_direction.x != 0:
+			next_anim = "idle_left"
+			set_flip(last_direction.x > 0)
+		elif last_direction.y < 0:
+			next_anim = "idle_up"
+			set_flip(false)
+		elif last_direction.y > 0:
+			next_anim = "idle_down"
+			set_flip(false)
+	
+	# play only if animation changed
+	if next_anim != "" and animation_player.current_animation != next_anim:
+		animation_player.play(next_anim)
+		
+	
+	#stop animation
+		# update direction and velocity 
+	if direction.x != 0:
+		velocity.x = direction.x * speed
+	else:
+		velocity.x = 0.0
+
+	if direction.y != 0:
+		velocity.y = direction.y * speed
+	else:
+		velocity.y = 0.0
+	
 	move_and_slide()
+	
+func initialize_player():
+	
+		# body and colour
+	body.texture = Global.bodies_collection_BIG[Global.selected_body_BIG]
+	body.modulate = Global.selected_body_colour_BIG
+
+	# hair and colour
+	hair.texture = Global.hair_collection_BIG[Global.selected_hair_BIG]
+	hair.modulate = Global.selected_hair_colour
+	
+	# outfit 
+	outfit.texture = Global.outfit_collection_BIG[Global.selected_outfit_BIG]
+
+# helper to flip horizontally
+func set_flip(should_flip: bool):
+	body.flip_h = should_flip
+	hair.flip_h = should_flip
+	outfit.flip_h = should_flip
