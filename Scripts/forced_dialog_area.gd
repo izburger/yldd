77b7d08e97805c560c14b84b997465e2a965fbdd
izburger <144290998+izburger@users.noreply.github.com>
2025-10-23 18:19:41 +1300

    clean commit just to save

--- Scripts/forced_dialog_area.gd
+++ Scripts/forced_dialog_area.gd
@@ -1,14 +1,15 @@
 extends Area2D
 
 @export var dialog_key = ""
-@export var dia_k = ""
 var area_active = false
+var played = false
 
 func _ready():
 	set_process_input(true)
 
 func _input(event):
-	if area_active
+	if area_active and not played:
+		played = true
 		SignalBus.emit_signal("display_dialog", dialog_key)
 		
 func _on_area_entered(area: Area2D) -> void:
