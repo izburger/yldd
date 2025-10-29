8bfa4f4b9c0c0376a3559d224301086ccaf913b1
izburger <144290998+izburger@users.noreply.github.com>
2025-10-29 23:30:44 +1300

    First floor correct spawns

--- Scripts/door_area.gd
+++ Scripts/door_area.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @export_file("*.tscn") var target_scene: String
+@export var exit_spawn_id: String = ""
 @onready var prompt: Label = $PromptLabel
 
 var _player_inside := false
@@ -22,17 +23,19 @@ func _on_area_exited(area: Area2D) -> void:
 
 func _unhandled_input(event: InputEvent) -> void:
 	if _player_inside and Input.is_action_just_pressed("interact"):
-		#_cooldown = true
+		if target_scene == "" or exit_spawn_id == "":
+			push_error("Door missing target_scene or exit_spawn_id")
+			return
 		prompt.visible = false
-		_change_scene_safe()
+		GameStates.change_scene_with_spawn(target_scene, exit_spawn_id)
 
-func _change_scene_safe() -> void:
-	get_tree().paused = false
-	call_deferred("_do_change")
+#func _change_scene_safe() -> void:
+#	get_tree().paused = false
+#	call_deferred("_do_change")
 
-func _do_change() -> void:
-	if target_scene == "" or not FileAccess.file_exists(target_scene):
-		push_error("DoorArea2D: target_scene not set or missing.")
-		#_cooldown = false
-		return
-	get_tree().change_scene_to_file(target_scene)
+#func _do_change() -> void:
+#	if target_scene == "" or not FileAccess.file_exists(target_scene):
+#		push_error("DoorArea2D: target_scene not set or missing.")
+#		#_cooldown = false
+#		return
+#	get_tree().change_scene_to_file(target_scene)
